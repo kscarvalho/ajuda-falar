542f184018a91c4c8f30c29a98ba904f805140b5
Francisco Carvalho <franciscoklebioks@gmail.com>
2025-03-23 02:06:49 -0300

    som dog js

---
+++ animais.js
@@ -0,0 +1,7 @@
+// Seleciona todas as imagens com a classe 'animal-img'
+const cao = document.querySelector('.cao');
+
+cao.addEventListener('click', function() {
+  const audio = document.querySelector('audio')
+  audio.play()
+})
