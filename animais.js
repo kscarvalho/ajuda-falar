b7aa0b28171eca5f5fe0eb24bd7500f7151869bc
Francisco Carvalho <franciscoklebioks@gmail.com>
2025-04-05 07:53:33 -0300

    audio

--- animais.js
+++ animais.js
@@ -5,11 +5,3 @@ cao.addEventListener('click', function() {
   const audio = document.querySelector('audio')
   audio.play()
 })
-
-// Seleciona todas as imagens com a classe 'animal-img'
-const cao = document.querySelector('.cachorro');
-
-cao.addEventListener('click', function() {
-  const audio = document.querySelector('audio')
-  audio.play()
-})
