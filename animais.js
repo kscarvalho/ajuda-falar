0975dffdbd52b6a882093f66814fc2a571a4cbb4
Francisco Carvalho <franciscoklebioks@gmail.com>
2025-04-05 07:49:35 -0300

    add audio cachorro

--- animais.js
+++ animais.js
@@ -1,5 +1,13 @@
 // Seleciona todas as imagens com a classe 'animal-img'
-const cao = document.querySelector('.vaca');
+const vaca = document.querySelector('.vaca');
+
+cao.addEventListener('click', function() {
+  const audio = document.querySelector('audio')
+  audio.play()
+})
+
+// Seleciona todas as imagens com a classe 'animal-img'
+const cao = document.querySelector('.cachorro');
 
 cao.addEventListener('click', function() {
   const audio = document.querySelector('audio')
