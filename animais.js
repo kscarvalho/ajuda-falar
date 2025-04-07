b291505c8c758c9a36464a13221876b1a2f59671
Francisco Carvalho <franciscoklebioks@gmail.com>
2025-04-06 23:29:25 -0300

    add sons as imgs

--- animais.js
+++ animais.js
@@ -1,7 +1,139 @@
-// Seleciona todas as imagens com a classe 'animal-img'
-const vaca = document.querySelector('.vaca');
+const imagem1 = document.querySelectorAll('.vaca');
 
-cao.addEventListener('click', function() {
-  const audio = document.querySelector('audio')
-  audio.play()
-})
+const somVaca = () => {
+  const somVaca = document.getElementById('vaca');
+  somVaca.play();
+};
+
+imagem1.forEach(imagem => {
+  imagem.addEventListener('click', somVaca)
+});
+
+const imagem2 = document.querySelectorAll('.cachorro');
+
+const somCachorro = () => {
+  const somCachorro = document.getElementById('cachorro');
+  somCachorro.play();
+};
+
+imagem2.forEach(imagem => {
+  imagem.addEventListener('click', somCachorro)
+});
+
+
+const imagem3 = document.querySelectorAll('.ovelha');
+
+const somOvelha = () => {
+  const somOvelha = document.getElementById('ovelha');
+  somOvelha.play();
+};
+
+imagem3.forEach(imagem => {
+  imagem.addEventListener('click', somOvelha)
+});
+
+const imagem4 = document.querySelectorAll('.leao');
+
+const somLeao = () => {
+  const somLeao = document.getElementById('leao');
+  somLeao.play();
+};
+
+imagem4.forEach(imagem => {
+  imagem.addEventListener('click', somLeao)
+});
+
+
+const imagem5 = document.querySelectorAll('.arara');
+
+const somArara = () => {
+  const somArara = document.getElementById('arara');
+  somArara.play();
+};
+
+imagem5.forEach(imagem => {
+  imagem.addEventListener('click', somArara)
+});
+
+
+const imagem6 = document.querySelectorAll('.girafa');
+
+const somGirafa = () => {
+  const somGirafa = document.getElementById('girafa');
+  somGirafa.play();
+};
+
+imagem6.forEach(imagem => {
+  imagem.addEventListener('click', somGirafa)
+});
+
+const imagem7 = document.querySelectorAll('.boi');
+
+const somBoi = () => {
+  const somBoi = document.getElementById('boi');
+  somBoi.play();
+};
+
+imagem7.forEach(imagem => {
+  imagem.addEventListener('click', somBoi)
+});
+
+const imagem8 = document.querySelectorAll('.galinha');
+
+const somGalinha = () => {
+  const somGalinha = document.getElementById('galinha');
+  somGalinha.play();
+};
+
+imagem8.forEach(imagem => {
+  imagem.addEventListener('click', somGalinha)
+});
+
+const imagem9 = document.querySelectorAll('.gato');
+
+const somGato = () => {
+  const somGato = document.getElementById('gato');
+  somGato.play();
+};
+
+imagem9.forEach(imagem => {
+  imagem.addEventListener('click', somGato)
+});
+
+const imagem10 = document.querySelectorAll('.pato');
+
+const somPato = () => {
+  const somPato = document.getElementById('pato');
+  somPato.play();
+};
+
+imagem10.forEach(imagem => {
+  imagem.addEventListener('click', somPato)
+});
+
+const imagem11 = document.querySelectorAll('.pinto');
+
+const somPinto = () => {
+  const somPinto = document.getElementById('pinto');
+  somPinto.play();
+};
+
+imagem11.forEach(imagem => {
+  imagem.addEventListener('click', somPinto)
+});
+
+
+const imagem12 = document.querySelectorAll('.porco');
+
+const somPorco = () => {
+  const somPorco = document.getElementById('porco');
+  somPorco.play();
+};
+
+imagem12.forEach(imagem => {
+  imagem.addEventListener('click', somPorco)
+});
+
+
+
+
