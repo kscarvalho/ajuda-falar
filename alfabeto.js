b293c8deb63c9246cca25cb4f091d86a95696371
Francisco Carvalho <franciscoklebioks@gmail.com>
2025-05-04 16:38:25 -0300

    criado area alfabeto

---
+++ alfabeto.js
@@ -0,0 +1,129 @@
+const som1 = document.querySelectorAll('.somA');
+
+const somA = () => {
+  const somA = document.getElementById('a');
+  somA.play();
+};
+
+som1.forEach(som => {
+  som.addEventListener('click', somA)
+});
+
+
+
+const som2 = document.querySelectorAll('.somB');
+
+const somB = () => {
+  const somB = document.getElementById('b');
+  somB.play();
+};
+
+som2.forEach(som => {
+  som.addEventListener('click', somB)
+});
+
+const som3 = document.querySelectorAll('.somC');
+
+const somC = () => {
+  const somC = document.getElementById('c');
+  somC.play();
+};
+
+som3.forEach(som => {
+  som.addEventListener('click', somC)
+});
+
+const som4 = document.querySelectorAll('.somD');
+
+const somD = () => {
+  const somD = document.getElementById('d');
+  somD.play();
+};
+
+som4.forEach(som => {
+  som.addEventListener('click', somD)
+});
+
+const som5 = document.querySelectorAll('.somE');
+
+const somE = () => {
+  const somE = document.getElementById('e');
+  somE.play();
+};
+
+som5.forEach(som => {
+  som.addEventListener('click', somE)
+});
+
+const som6 = document.querySelectorAll('.somF');
+
+const somF = () => {
+  const somF = document.getElementById('f');
+  somF.play();
+};
+
+som6.forEach(som => {
+  som.addEventListener('click', somF)
+});
+
+const som7 = document.querySelectorAll('.somG');
+
+const somG = () => {
+  const somG = document.getElementById('g');
+  somG.play();
+};
+
+som7.forEach(som => {
+  som.addEventListener('click', somG)
+});
+
+const som8 = document.querySelectorAll('.somH');
+
+const somH = () => {
+  const somH = document.getElementById('h');
+  somH.play();
+};
+
+som8.forEach(som => {
+  som.addEventListener('click', somH)
+});
+
+const som9 = document.querySelectorAll('.somI');
+
+const somI = () => {
+  const somI = document.getElementById('i');
+  somI.play();
+};
+
+som9.forEach(som => {
+  som.addEventListener('click', somI)
+});
+
+const som10 = document.querySelectorAll('.somJ');
+
+const somJ = () => {
+  const somJ = document.getElementById('j');
+  somJ.play();
+};
+
+som10.forEach(som => {
+  som.addEventListener('click', somJ)
+});
+
+const som11 = document.querySelectorAll('.somL');
+
+const somL = () => {
+  const somL = document.getElementById('l');
+  somL.play();
+};
+
+som11.forEach(som => {
+  som.addEventListener('click', somL)
+});
+
+
+
+
+
+
+
